b0be9f255658ee3da31658194299eb8334865ecc
douglasdans <douglas.dans@gmail.com>
2024-02-01 20:10:02 -0300

    Autor | Criado Classe e Persistência

---
+++ src/main/java/com/livros/livros/model/Autor.java
@@ -0,0 +1,27 @@
+package com.livros.livros.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/livros/livros/model/AutorRepository.java
@@ -0,0 +1,7 @@
+package com.livros.livros.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
