bedc2443fdb0b4965e84640e35efb9762102a9b3
douglasdans <douglas.dans@gmail.com>
2024-02-12 21:58:36 -0300

    HttpResponse | Refatorado

--- src/main/java/com/livros/livros/controller/exceptions/DatabaseExceptionHandler.java
+++ src/main/java/com/livros/livros/controller/exceptions/DatabaseExceptionHandler.java
@@ -1,41 +1,40 @@
 package com.livros.livros.controller.exceptions;
 
+import com.livros.livros.controller.response.HttpResponse;
 import com.livros.livros.service.exception.DatabaseException;
 import com.livros.livros.service.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class DatabaseExceptionHandler {
 
     Logger log = LogManager.getLogger(getClass());
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> dataNotFound(ResourceNotFoundException e, HttpServletRequest req) {
+    public ResponseEntity<HttpResponse> dataNotFound(ResourceNotFoundException e, HttpServletRequest req) {
         String error = "Dado não encontrado no banco de dados";
 
-        ErrorResponse httpError = new ErrorResponse(HttpStatus.NOT_FOUND, error, e.getMessage(), req.getRequestURI());
+        HttpResponse httpError = new HttpResponse(HttpStatus.NOT_FOUND, e.getMessage(), req.getRequestURI());
 
         log.error(">>>> [DatabaseExceptionHandler] " + error + " - Path: " + httpError.getPath());
 
-        return new ResponseEntity<ErrorResponse>(httpError, httpError.getStatus());
+        return new ResponseEntity<HttpResponse>(httpError, httpError.getStatus());
     }
 
     @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<ErrorResponse> databaseException(DatabaseException e, HttpServletRequest req) {
+    public ResponseEntity<HttpResponse> databaseException(DatabaseException e, HttpServletRequest req) {
         String error = "A requisição não pôde ser realizada pelo banco de dados";
 
-        ErrorResponse httpError = new ErrorResponse(HttpStatus.BAD_REQUEST, error, e.getMessage(), req.getRequestURI());
+        HttpResponse httpError = new HttpResponse(HttpStatus.BAD_REQUEST, e.getMessage(), req.getRequestURI());
 
         log.error(">>>> [DatabaseExceptionHandler] " + error + " - Path: " + httpError.getPath());
 
-        return new ResponseEntity<ErrorResponse>(httpError, httpError.getStatus());
+        return new ResponseEntity<HttpResponse>(httpError, httpError.getStatus());
     }
 
 
--- src/main/java/com/livros/livros/controller/response/HttpResponse.java
+++ src/main/java/com/livros/livros/controller/response/HttpResponse.java
@@ -1,26 +1,22 @@
-package com.livros.livros.controller.exceptions;
+package com.livros.livros.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
 
 import java.time.Instant;
 
-public class ErrorResponse {
+public class HttpResponse {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-    private Instant timestamp;
-
+    private final Instant timestamp = Instant.now();
     private HttpStatus status;
-    private String error;
     private String message;
     private String path;
 
-    public ErrorResponse() {}
+    public HttpResponse() {}
 
-    public ErrorResponse(HttpStatus status, String error, String message, String path) {
-        this.timestamp = Instant.now();
+    public HttpResponse(HttpStatus status, String message, String path) {
         this.status = status;
-        this.error = error;
         this.message = message;
         this.path = path;
     }
@@ -29,9 +25,6 @@ public class ErrorResponse {
         return timestamp;
     }
 
-    public void setTimestamp(Instant timestamp) {
-        this.timestamp = timestamp;
-    }
 
     public HttpStatus getStatus() {
         return status;
@@ -39,14 +32,6 @@ public class ErrorResponse {
 
     public void setStatus(HttpStatus status) {
         this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
     }
 
     public String getMessage() {
