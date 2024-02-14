69a4cb7a86a6a1d479ff21771fd53c5915889a88
douglasdans <douglas.dans@gmail.com>
2024-02-14 20:19:08 -0300

    LoadDatabase | Adição - Adicionado 1 Livro para popular o banco de dados para testes

--- src/main/java/com/livros/livros/config/LoadDatabase.java
+++ src/main/java/com/livros/livros/config/LoadDatabase.java
@@ -2,8 +2,10 @@ package com.livros.livros.config;
 
 import com.livros.livros.model.entities.Autor;
 import com.livros.livros.model.entities.Editora;
+import com.livros.livros.model.entities.Livro;
 import com.livros.livros.model.repositories.IAutorRepository;
 import com.livros.livros.model.repositories.IEditoraRepository;
+import com.livros.livros.model.repositories.ILivroRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -21,7 +23,8 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(
             IAutorRepository autorRepository,
-            IEditoraRepository editoraRepository
+            IEditoraRepository editoraRepository,
+            ILivroRepository livroRepository
     ) {
         return args -> {
             Autor autor1 = new Autor("Valdir Azevedo", "Brasileiro", "1967-10-06");
@@ -29,6 +32,18 @@ public class LoadDatabase {
 
             Editora editora1 = new Editora("Saraiva", "91643714000173", "Brasil");
             editoraRepository.saveAll(Arrays.asList(editora1));
+
+            Livro livro1 = new Livro(
+                    "9788532511015",
+                    "Harry Potter e a Pedra Filosofal",
+                    "O livro conta a história de Harry Potter, um órfão criado pelos tios que descobre, em seu décimo primeiro aniversário, que é um bruxo.",
+                    "1999-10-10",
+                    1,
+                    autor1,
+                    editora1
+            );
+
+            livroRepository.saveAll(Arrays.asList(livro1));
 
 
             log.info(">>>> [LoadDatabase] dados inseridos no Banco de dados");
