2bbdfc18f34811a466ba6d34dcb80a00ed9d0b31
douglasdans <douglas.dans@gmail.com>
2024-02-14 15:46:13 -0300

    EditoraAPIController | Criado Controller para editora

--- src/main/java/com/livros/livros/controller/EditoraAPIController.java
+++ src/main/java/com/livros/livros/controller/EditoraAPIController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
