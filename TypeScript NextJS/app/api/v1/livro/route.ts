e6e101cb590b34c919d4891552066b22bba2637c
douglasdans <douglas.dans@gmail.com>
2024-06-24 16:26:12 -0300

    Aterações - Remove páginas frontend do projeto - Cria estrutura de rotas em backend - Adiciona schema prisma ao projeto

--- TypeScript NextJS/app/api/v1/livro/route.ts
+++ TypeScript NextJS/app/api/v1/livro/route.ts
@@ -1,4 +1,8 @@
 export const dynamic = 'force-dynamic' // defaults to auto
 export async function GET(request: Request){
-   return new Response(JSON.stringify(null))
+   const obj = {
+      mensagem: "Olá Mundo"
+   }
+
+   return new Response(JSON.stringify(obj))
 }
