4a9604995546d8afec85612657fde96d7acd90f9
douglasdans <douglas.dans@gmail.com>
2024-07-02 15:18:52 -0300

    Adiciona Lógica de CRUD do livro

--- TypeScript NestJS/src/livro/livro.service.ts
+++ TypeScript NestJS/src/livro/livro.service.ts
@@ -1,29 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Livro } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { CreateLivroDto } from './dto/create-livro.dto';
+import { UpdateLivroDto } from './dto/update-livro.dto';
 
 @Injectable()
 export class LivroService {
 
   constructor(private prisma : PrismaService) {}
 
-  create(createLivroDto: Livro) {
-    return 'This action adds a new livro';
+  create(livro: CreateLivroDto) {
+    console.log(livro);
+    return this.prisma.livro.create({
+      data: livro,
+    });
   }
 
-  findAll() {
-    return `This action returns all livro`;
+  findAll() : Promise<Livro[]> {
+    return this.prisma.livro.findMany()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} livro`;
+    return this.prisma.livro.findFirstOrThrow({
+      where: {id: id}
+    }).catch(e => {
+      throw new HttpException({
+        message: "Dados não foram encontrados no banco - " + e,
+        statusCode: HttpStatus.NOT_FOUND
+      }, HttpStatus.NOT_FOUND)
+    })
   }
 
-  update(id: number, updateLivroDto: Livro) {
-    return `This action updates a #${id} livro`;
+  update(id: number, updateLivroDto: UpdateLivroDto) {
+    return this.prisma.livro.update({
+      where: {id: id},
+      data: updateLivroDto
+    }).catch(e => {
+      throw new HttpException({
+        message: "Dados não foram encontrados no banco - " + e.meta.cause,
+        statusCode: HttpStatus.NOT_FOUND
+      }, HttpStatus.NOT_FOUND)
+    })
   }
 
   remove(id: number) {
-    return `This action removes a #${id} livro`;
+    return this.prisma.livro.delete({
+      where: {id: id}
+    }).catch(e => {
+      throw new HttpException({
+        message: "Dados não foram encontrados no banco - " + e.meta.cause,
+        statusCode: HttpStatus.NOT_FOUND
+      }, HttpStatus.NOT_FOUND)
+    })
   }
 }
