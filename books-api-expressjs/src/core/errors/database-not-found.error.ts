0399ce39d6d4dd8b6603677b13740e4a4dfd2fd3
douglasdans <douglas.dans@gmail.com>
2025-02-18 15:47:33 -0300

    refactor: renomeia classe de erro para DatabaseDataNotFoundError

--- books-api-expressjs/src/core/errors/database-not-found.error.ts
+++ books-api-expressjs/src/core/errors/database-not-found.error.ts
@@ -1,4 +1,4 @@
-export class DatabaseDataNotFound extends Error {
+export class DatabaseDataNotFoundError extends Error {
   constructor() {
     super("Nenhum dado foi encontrado no banco de dados");
   }
