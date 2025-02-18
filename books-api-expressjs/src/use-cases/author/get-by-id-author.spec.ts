d2f2aeef2ec0500a40ce14929cbb97d7b721ffa9
douglasdans <douglas.dans@gmail.com>
2025-02-18 14:44:46 -0300

    test: adiciona testes para a funcionalidade de obter autor por ID

---
+++ books-api-expressjs/src/use-cases/author/get-by-id-author.spec.ts
@@ -0,0 +1,39 @@
+import { MockAuthorRepository } from "@/tests/mock-repositories/author.repository.mock";
+import { describe, expect, it } from "vitest";
+import { GetAuthorById } from "./get-by-id-author";
+import { Author } from "@/core/entities/author.entity";
+
+describe("GetAuthorById", () => {
+  it("deve retornar um objeto de Author de acordo com o ID", async () => {
+    const mockAuthorRepository = new MockAuthorRepository();
+    const getAuthorById = new GetAuthorById(mockAuthorRepository);
+
+    const author = await getAuthorById.execute(1);
+
+    expect(author).toBeInstanceOf(Author);
+
+    expect(author).toHaveProperty("id");
+    expect(author).toHaveProperty("name");
+    expect(author).toHaveProperty("nationality");
+    expect(author).toHaveProperty("birthDate");
+    expect(author).toHaveProperty("books");
+  });
+
+  it("deve retornar nulo de acordo com o ID", async () => {
+    const mockAuthorRepository = new MockAuthorRepository();
+    const getAuthorById = new GetAuthorById(mockAuthorRepository);
+
+    const author = await getAuthorById.execute(7);
+
+    expect(author).toBeNull();
+  });
+
+  it("deve retornar nulo para IDs negativos", async () => {
+    const mockAuthorRepository = new MockAuthorRepository();
+    const getAuthorById = new GetAuthorById(mockAuthorRepository);
+
+    const author = await getAuthorById.execute(-24);
+
+    expect(author).toBeNull();
+  });
+});
