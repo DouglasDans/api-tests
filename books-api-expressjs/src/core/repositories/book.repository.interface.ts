be89dea7022121f0c58df84d8df321abde026c9c
douglasdans <douglas.dans@gmail.com>
2025-02-20 21:20:02 -0300

    refactor: altera métodos getById nas interfaces de repositório para retornar objetos ao invés de null

--- books-api-expressjs/src/core/repositories/book.repository.interface.ts
+++ books-api-expressjs/src/core/repositories/book.repository.interface.ts
@@ -3,7 +3,7 @@ import { Book } from "../entities/book.entity";
 export default interface IBookRepository {
   create(book: Omit<Book, "id">): Promise<Book>;
   getAll(): Promise<Book[]>;
-  getById(id: number): Promise<Book | null>;
+  getById(id: number): Promise<Book>;
   update(book: Book): Promise<Book>;
   delete(id: number): Promise<Book>;
 }
--- books-api-expressjs/src/core/repositories/publisher.repository.interface.ts
+++ books-api-expressjs/src/core/repositories/publisher.repository.interface.ts
@@ -3,7 +3,7 @@ import { Publisher } from "../entities/publisher.entity";
 export default interface IPublisherRepository {
   create(publisher: Omit<Publisher, "id">): Promise<Publisher>;
   getAll(): Promise<Publisher[]>;
-  getById(id: number): Promise<Publisher | null>;
+  getById(id: number): Promise<Publisher>;
   update(publisher: Publisher): Promise<Publisher>;
   delete(id: number): Promise<Publisher>;
 }
