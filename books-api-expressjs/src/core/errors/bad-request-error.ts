1f333cfaa8c1b168ce6f22fe919f6fb02d1f4285
douglasdans <douglas.dans@gmail.com>
2025-02-22 20:41:42 -0300

    fix: corrige o código de status da classe BadRequestError para 400

--- books-api-expressjs/src/core/errors/bad-request-error.ts
+++ books-api-expressjs/src/core/errors/bad-request-error.ts
@@ -3,7 +3,7 @@ import HttpError from "./http-error-response";
 export class BadRequestError extends HttpError {
   constructor(errorMessage?: string) {
     super({
-      statusCode: 500,
+      statusCode: 400,
       name: "BadRequestError",
       message: errorMessage || "Request is invalid",
     });
