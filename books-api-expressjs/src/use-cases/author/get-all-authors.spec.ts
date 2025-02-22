aceba5d36b76b3aaabb00bb32c347392fd2c6f38
douglasdans <douglas.dans@gmail.com>
2025-02-22 17:42:22 -0300

    feat: adiciona verificação de tipo para autores na especificação de recuperação de todos os autores

--- books-api-expressjs/src/use-cases/author/get-all-authors.spec.ts
+++ books-api-expressjs/src/use-cases/author/get-all-authors.spec.ts
@@ -1,6 +1,7 @@
 import { MockAuthorRepository } from "@/tests/mock-repositories/author.repository.mock";
 import { describe, expect, expectTypeOf, it } from "vitest";
 import { GetAllAuthors } from "./get-all-authors";
+import { Author } from "@/core/entities/author.entity";
 
 describe("GetAllAuthors", () => {
   const mockAuthorRepository = new MockAuthorRepository();
@@ -10,5 +11,6 @@ describe("GetAllAuthors", () => {
 
     expect(mockAuthorRepository.getAll).toBeCalledTimes(1);
     expect(result.length).toBeGreaterThanOrEqual(0);
+    expect(result[0]).toBeInstanceOf(Author);
   });
 });
