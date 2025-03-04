283f0e3a8bdad5c27791f5a38dcff413c28f2277
douglasdans <douglas.dans@gmail.com>
2025-03-04 18:52:00 -0300

    feat: adiciona camada de domínio seguindo o diagrama e o curso da DIO

---
+++ bank-api-spring/src/main/java/dev/dans/bank/domain/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.dans.bank.domain.repositories;
+
+import dev.dans.bank.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String accountNumber);
+}
