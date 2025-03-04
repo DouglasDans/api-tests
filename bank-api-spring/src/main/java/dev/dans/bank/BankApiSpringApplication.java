017c05fa7fd90f5ee8feeba4eb6ed8541e6cc088
douglasdans <douglas.dans@gmail.com>
2025-03-04 18:00:55 -0300

    adiciona base de projeto de banco em spring

---
+++ bank-api-spring/src/main/java/dev/dans/bank/BankApiSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.dans.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiSpringApplication.class, args);
+    }
+
+}
