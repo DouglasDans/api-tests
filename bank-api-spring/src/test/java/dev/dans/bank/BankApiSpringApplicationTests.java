fb3ae8ef22bfb242d7bf46692d855d0b001c112c
douglasdans <douglas.dans@gmail.com>
2025-03-04 18:04:39 -0300

    feat: adiciona arquivo de testes

---
+++ bank-api-spring/src/test/java/dev/dans/bank/BankApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dans.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
